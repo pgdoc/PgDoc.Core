b173f86b59ca200910125d4db651bcdddceb2a6d
Flavien Charlon <flavien@charlon.org>
2016-04-01 18:13:57 +0100

    Add a license header to source files

--- PgDoc/src/PgDoc/SQL/pgdoc.sql
+++ PgDoc/src/PgDoc/SQL/pgdoc.sql
@@ -1,3 +1,21 @@
+-- Copyright 2016 Flavien Charlon
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--     http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+-- ======================================================================
+-- document: Contains all the documents.
+-- ======================================================================
+
 CREATE TABLE document
 (
     id      uuid PRIMARY KEY,
@@ -6,10 +24,9 @@ CREATE TABLE document
     version bytea NOT NULL
 );
 
----------------------------------------------------
-
----------------------------------------------------
---- Update a series of documents
+-- ======================================================================
+-- update_documents: Updates a series of documents.
+-- ======================================================================
 
 CREATE TYPE document_update AS
 (
@@ -70,8 +87,9 @@ BEGIN
 
 END $$ LANGUAGE plpgsql;
 
----------------------------------------------------
---- Get a list of documents from their IDs
+-- ======================================================================
+-- get_documents: Gets a list of documents from their IDs.
+-- ======================================================================
 
 CREATE OR REPLACE FUNCTION get_documents(ids uuid[])
 RETURNS TABLE (id uuid, body jsonb, version bytea) AS $$
